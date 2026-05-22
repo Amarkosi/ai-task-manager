c05b0623f15b6c1f8c882d0760259358809d7907
Andranik Markosyan <142671173+Amarkosi@users.noreply.github.com>
2026-05-22 20:34:57 +0400

    main.py

--- backend/backend_app/main.py
+++ backend/backend_app/main.py
@@ -11,13 +11,18 @@ from backend_app import models
 Base.metadata.create_all(bind=engine)
 app = FastAPI(title="AI Task Manager API")
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://ai-task-manager-sand.vercel.app/" 
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 class ConnectionManager:
