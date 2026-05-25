cad2ba0508de8bd73d419a188dab5829a02a2dc6
Andranik Markosyan <142671173+Amarkosi@users.noreply.github.com>
2026-05-25 16:41:05 +0400

    database.py

--- backend/backend_app/database.py
+++ backend/backend_app/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 # URL для локального подключения к PostgreSQL из Docker
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/task_db")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
